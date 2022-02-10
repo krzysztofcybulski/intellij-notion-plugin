6b916d459a7efe0e36ecac7f6a469ba83ef5e0a9
GitHub Action <action@github.com>
2022-02-10 13:11:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/krzysztofcybulski/intellijnotionplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.krzysztofcybulski.intellijnotionplugin.services
+
+import com.github.krzysztofcybulski.intellijnotionplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/krzysztofcybulski/intellijnotionplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/krzysztofcybulski/intellijnotionplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.krzysztofcybulski.intellijnotionplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.krzysztofcybulski.intellijnotionplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
